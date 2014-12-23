cd8d8391fd7e2a6533021dfd7f4d6be4ab59623f
Shuhao Wu <shuhao@shuhaowu.com>
2014-12-23 16:30:14 +0000

    Left handed navbar during landscape mode
    This patch allows for the navbar to be moved to the left side of the
    screen during landscape mode. Left handed people are more used to this
    option.
    
    This patch must go with another patch that changes
    packages/apps/Settings
    
    Screenshot: http://i.imgur.com/jWqJcEV.png
    
    Change-Id: Ic95e3fda1f55efd2ec908c8d7226e638ec0ab80a
    
    Expanded Desktop : Fix Left handed navbar during landscape mode
    
    from http://review.cyanogenmod.org/#/c/57454
    when you enable expanded desktop and left handed navbar.
    you need to swipe from right edge to show navbar.
    but this patch will change to swipe from left edge to show navbar.
    Video avalible at http://youtu.be/rUy14LQj9iE
    
    Patchset2 : change tab to whitespace
    Patchset3 : remove \r after @override in PhoneWindowManager.java
    Patchset4 : Update commit message
    Patchset5 : Address code formating comments
    Patchset6 : Whitespace
    
    Altaf-Mahdi
    Add left gesture for edge gesture service
    
    *** Changes for DU ***
    
    -- Don't observer URI in PhoneStatusbar. We implement settings change in our NavigationController
    
    Change-Id: I65a15c6f5e662e429a818484de6b71e81c7ae713

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3600,6 +3600,12 @@ public final class Settings {
         public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
 
         /**
+         * Whether navigation bar is placed on the left side in landscape mode
+         * @hide
+         */
+        public static final String NAVBAR_LEFT_IN_LANDSCAPE = "navigation_bar_left";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
