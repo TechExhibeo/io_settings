dbe53cb3d5c02b616c681c53c519e01241094bab
Jackeagle <jackeagle102@gmail.com>
2015-05-03 00:57:31 -0400

    Add Infinitive World To Settings
    - Using Viewpager layout
    - Add Navigation Settings as Fragment
    
    PS2 : Add About InfinitiveOS
    PS3 : Add About InfinitiveOS as Fragment
    
    Credits :
    @snak3ater Commit : https://github.com/SAOSP-L/platform_packages_apps_Settings/commit/74513dd8aa18d1094205aa0df2b3b1129c875738
    @alviteri Commit : https://github.com/crdroidandroid/android_packages_apps_Settings/commit/5cca0fcc1b308e171b7f9d2c21079a8dd2997d45
    
    Change-Id: I237c1f5542837281bd99855d68ba613fcb2df222
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    
    Conflicts:
    	AndroidManifest.xml
    	res/values/styles.xml
    	res/xml/display_settings.xml
    
    Conflicts:
    	AndroidManifest.xml

---
+++ src/com/android/settings/io/About.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2015 crDroid Android
+ * Copyright (C) 2015 InfinitiveOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.io;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceGroup;
+import android.preference.PreferenceScreen;
+
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class About extends SettingsPreferenceFragment {
+
+    public static final String TAG = "About";
+
+    Preference mSiteUrl;
+    Preference mSourceUrl;
+    Preference mGerritUrl;
+    Preference mGoogleUrl;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.io_about);
+
+        mSiteUrl = findPreference("io_website");
+        mSourceUrl = findPreference("io_source");
+        mGerritUrl = findPreference("io_gerrit");
+        mGoogleUrl = findPreference("io_google_plus");
+
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mSiteUrl) {
+            launchUrl("http://aogp-devs.com/");
+        } else if (preference == mSourceUrl) {
+            launchUrl("https://github.com/InfinitiveOS");
+        } else if (preference == mGerritUrl) {
+            launchUrl("http://gerrit.aogp-devs.com");
+        } else if (preference == mGoogleUrl) {
+            launchUrl("");
+        }
+
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    private void launchUrl(String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent donate = new Intent(Intent.ACTION_VIEW, uriUrl);
+        getActivity().startActivity(donate);
+    }
+}
