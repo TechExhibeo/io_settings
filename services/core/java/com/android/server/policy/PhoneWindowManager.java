07d26b3ce83c1a56daff22a881770bf76d80fbcc
Jorge Ruesga <jorge@ruesga.com>
2015-09-13 21:12:51 +0200

    base: fix window bounds for left hand navigationbar
    Currently in left-hand navigationbar mode all drawerlayouts have an
    inconsistent presentation because the actual window size calculation
    code only takes into consideration the content view. This problem is not
    present in the rest of the navigation modes because of the nature of the
    position of the navigation bar. This patch fix the display and content
    layouts calculation.  To repro just set navigation mode to hand-left and
    access an app with a drawerlayout (xe:the email app) in landscape mode.
    
    Change-Id: I1394f39f4f5e7e6ac50b9558657685e86547acf3
    TICKET: CYNGNOS-1748
    
    Conflicts:
    	services/core/java/com/android/server/policy/PhoneWindowManager.java
    
    Change-Id: I5dfbcfc535c6d67db0c41c4303c9aea67e830891

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -4500,7 +4500,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 ? attached.getFrameLw() : df);
     }
 
-    private void applyStableConstraints(int sysui, int fl, Rect r) {
+    private void applyStableConstraints(int sysui, int fl, Rect r, Rect d) {
+        if (mNavigationBarLeftInLandscape) {
+            d.left = r.left;
+            r.left = 0;
+        }
+
         if ((sysui & View.SYSTEM_UI_FLAG_LAYOUT_STABLE) != 0) {
             // If app is requesting a stable layout, don't let the
             // content insets go below the stable values.
@@ -4756,7 +4761,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                         cf.right = mRestrictedScreenLeft + mRestrictedScreenWidth;
                         cf.bottom = mRestrictedScreenTop + mRestrictedScreenHeight;
                     }
-                    applyStableConstraints(sysUiFl, fl, cf);
+                    applyStableConstraints(sysUiFl, fl, cf, df);
                     if (adjust != SOFT_INPUT_ADJUST_NOTHING) {
                         vf.left = mCurLeft;
                         vf.top = mCurTop;
@@ -4869,7 +4874,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                             + mRestrictedScreenHeight;
                 }
 
-                applyStableConstraints(sysUiFl, fl, cf);
+                applyStableConstraints(sysUiFl, fl, cf, df);
 
                 if (adjust != SOFT_INPUT_ADJUST_NOTHING) {
                     vf.left = mCurLeft;
