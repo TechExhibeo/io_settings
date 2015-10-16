4077b015b639d5f43c5bf1f9a4a739627f011d27
kecinzer <kecinzer@gmail.com>
2015-10-16 23:06:51 +0500

    Battery text mode (1/2)
    Squashed:
    Author: DvTonder
    Fix battery % always showing at < 100%
    https://github.com/CyanogenMod/android_frameworks_base/commit/a1bab878eb11ccb70df09c7876c7baa06fd0e49d
    
    Author:kecinzer
    Battery text mode (1/2)
    https://github.com/CyanogenMod/android_frameworks_base/commit/040721140b6dfe6a4ae5c0db7251073163f19b5d
    
    Author: EnderBlue
    Forward port battery icon options [1/2]
    https://github.com/CyanogenMod/android_frameworks_base/commit/0e31558822b1920ad39e4ba87f813b0bf823a61e
    
    Author: mikeNG
    Cleanup battery style code
    https://github.com/CyanogenMod/android_frameworks_base/commit/9203171b83f530b2948557d0207c70799308c879
    
    Author: maniac103
    Clean up battery handling.
    cffcdc5e20fc729174c650393409ca56e6408bee
    
    Author: LorDClockaN
    FWB: Dotted circle battery (1/2)
    https://github.com/AICP/frameworks_base/commit/84314f10bb2eb14ed560d95a7cf562029e18162c
    
    Some 6.0 changes and partial revert of commit:
    Tuner: add battery pct preference.
    https://github.com/android/platform_frameworks_base/commit/f40d08f8a3094ac5d5478efc5de0b96f57d2c5df
    
    Author: Jackeagle
    Revert "SystemUI: Show percentage of battery"
    This reverts https://github.com/Infinitive-OS/platform_frameworks_base/commit/5cec0d34be3a9555eca10356576846a620a67dc9
    TODO: setDarkIntensity in BatteryMeterView needs attention
    
    Change-Id: Ibfc0cf9c5a4efc13ecd3c3e99723bb2b717c50a7
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1816,7 +1816,6 @@ public class NotificationPanelView extends PanelView implements
 
     private void setListening(boolean listening) {
         mHeader.setListening(listening);
-        mKeyguardStatusBar.setListening(listening);
         mQsPanel.setListening(listening);
     }
 
