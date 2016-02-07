a077f6cc9799d9b3ac0f58e06916e203c058090c
Randall Rushing <randall.rushing@gmail.com>
2016-02-07 05:00:49 -0800

    Navbar: expose Fling and Smarbar settings [1/2]
    Change-Id: I0e02cb7cd6ab871f08ef049046ea789b6e15e6eb
    
    Conflicts:
    	AndroidManifest.xml
    	src/com/android/settings/SettingsActivity.java
    
    Conflicts:
    	src/com/android/settings/SettingsActivity.java

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -89,7 +89,9 @@ import com.android.settings.deviceinfo.PrivateVolumeForget;
 import com.android.settings.deviceinfo.PrivateVolumeSettings;
 import com.android.settings.deviceinfo.PublicVolumeSettings;
 import com.android.settings.deviceinfo.StorageSettings;
+import com.android.settings.dui.nav.FlingSettings;
 import com.android.settings.dui.nav.NavbarSettings;
+import com.android.settings.dui.nav.SmartBarSettings;
 import com.android.settings.fuelgauge.BatterySaverSettings;
 import com.android.settings.fuelgauge.PowerUsageDetail;
 import com.android.settings.fuelgauge.PowerUsageSummary;
@@ -359,7 +361,9 @@ public class SettingsActivity extends Activity
             ProcessStatsSummary.class.getName(),
             DrawOverlayDetails.class.getName(),
             WriteSettingsDetails.class.getName(),
-            NavbarSettings.class.getName()
+            NavbarSettings.class.getName(),
+            FlingSettings.class.getName(),
+            SmartBarSettings.class.getName()
     };
 
 
