4440480d688c8df74bf266f9e362396c60d15e5f
Deepak Sharma <TechExhibeo@gmail.com>
2016-04-27 04:52:03 +0200

    Revert "MSIM: Provide voice pref selection in qucksettings"
    This reverts commit fb8c4cb42a4e2803b8ff1ce81a8b5842314f82cc.

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -39,13 +39,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.internal.logging.MetricsLogger;
-import com.android.internal.telephony.CarrierAppUtils;
 import com.android.systemui.FontSizeUtils;
 import com.android.systemui.R;
 import com.android.systemui.cm.UserContentObserver;
 import com.android.systemui.qs.QSTile.DetailAdapter;
 import com.android.systemui.settings.BrightnessController;
-import com.android.systemui.settings.SimSwitchController;
 import com.android.systemui.settings.ToggleSlider;
 import com.android.systemui.statusbar.phone.QSTileHost;
 import com.android.systemui.statusbar.policy.BrightnessMirrorController;
@@ -65,7 +63,6 @@ public class QSPanel extends ViewGroup {
     private final TextView mDetailSettingsButton;
     private final TextView mDetailDoneButton;
     protected final View mBrightnessView;
-    protected View mSimSwitcherView = null;
     private final QSDetailClipper mClipper;
     private final H mHandler = new H();
 
@@ -90,12 +87,10 @@ public class QSPanel extends ViewGroup {
     private Record mDetailRecord;
     private Callback mCallback;
     private BrightnessController mBrightnessController;
-    private SimSwitchController mSimSwitchController;
     private QSTileHost mHost;
 
     private QSFooter mFooter;
     private boolean mGridContentVisible = true;
-    CarrierAppUtils.CARRIER carrier = CarrierAppUtils.getCarrierId();
 
     protected Vibrator mVibrator;
 
@@ -118,12 +113,6 @@ public class QSPanel extends ViewGroup {
         updateDetailText();
         mDetail.setVisibility(GONE);
         mDetail.setClickable(true);
-        if (isVoicePrefEnabled()) {
-            mSimSwitcherView = LayoutInflater.from(context).inflate(
-                    R.layout.sim_switcher, this, false);
-            addView(mSimSwitcherView);
-            mSimSwitchController = new SimSwitchController(getContext(), mSimSwitcherView, this);
-        }
         mBrightnessView = LayoutInflater.from(context).inflate(
                 R.layout.quick_settings_brightness_dialog, this, false);
         mFooter = new QSFooter(this, context);
@@ -535,15 +524,8 @@ public class QSPanel extends ViewGroup {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         final int width = MeasureSpec.getSize(widthMeasureSpec);
-
-        int simswitcherHeight = 0;
-        if (isVoicePrefEnabled()) {
-            mSimSwitcherView.measure(exactly(width), MeasureSpec.UNSPECIFIED);
-            simswitcherHeight = mSimSwitcherView.getMeasuredHeight() + mBrightnessPaddingTop;
-        }
         mBrightnessView.measure(exactly(width), MeasureSpec.UNSPECIFIED);
-        final int brightnessHeight =
-                mBrightnessView.getMeasuredHeight() + mBrightnessPaddingTop + simswitcherHeight;
+        final int brightnessHeight = mBrightnessView.getMeasuredHeight() + mBrightnessPaddingTop;
         mFooter.getView().measure(exactly(width), MeasureSpec.UNSPECIFIED);
         int r = -1;
         int c = -1;
@@ -592,18 +574,9 @@ public class QSPanel extends ViewGroup {
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         final int w = getWidth();
-        int padding = mBrightnessPaddingTop;
-        if (isVoicePrefEnabled()) {
-            if (mSimSwitcherView.getVisibility() != View.GONE) {
-                padding = mBrightnessPaddingTop*2 + mSimSwitcherView.getMeasuredHeight();
-            }
-            mSimSwitcherView.layout(0, mBrightnessPaddingTop,
-                    mSimSwitcherView.getMeasuredWidth(),
-                    mBrightnessPaddingTop + mSimSwitcherView.getMeasuredHeight());
-        }
-        mBrightnessView.layout(0, padding,
+        mBrightnessView.layout(0, mBrightnessPaddingTop,
                 mBrightnessView.getMeasuredWidth(),
-                padding + mBrightnessView.getMeasuredHeight());
+                mBrightnessPaddingTop + mBrightnessView.getMeasuredHeight());
         boolean isRtl = getLayoutDirection() == LAYOUT_DIRECTION_RTL;
         for (TileRecord record : mRecords) {
             if (record.tileView.getVisibility() == GONE) continue;
@@ -632,17 +605,9 @@ public class QSPanel extends ViewGroup {
     }
 
     private int getRowTop(int row) {
-        if (!isVoicePrefEnabled() || mSimSwitcherView.getVisibility() == View.GONE) {
-            if (row <= 0) return mBrightnessView.getMeasuredHeight() + mBrightnessPaddingTop;
-            return mBrightnessView.getMeasuredHeight() + mBrightnessPaddingTop
-                    + mLargeCellHeight - mDualTileUnderlap + (row - 1) * mCellHeight;
-        } else {
-            if (row <= 0) return mSimSwitcherView.getMeasuredHeight() + mBrightnessPaddingTop*2
-                    + mBrightnessView.getMeasuredHeight();
-            return mBrightnessView.getMeasuredHeight() + mBrightnessPaddingTop*2
-                    + mBrightnessView.getMeasuredHeight()
-                    + mLargeCellHeight - mDualTileUnderlap + (row - 1) * mCellHeight;
-        }
+        if (row <= 0) return mBrightnessView.getMeasuredHeight() + mBrightnessPaddingTop;
+        return mBrightnessView.getMeasuredHeight() + mBrightnessPaddingTop
+                + mLargeCellHeight - mDualTileUnderlap + (row - 1) * mCellHeight;
     }
 
     private int getColumnCount(int row) {
@@ -749,6 +714,7 @@ public class QSPanel extends ViewGroup {
         void onScanStateChanged(boolean state);
     }
 
+
     private class SettingsObserver extends UserContentObserver {
         SettingsObserver(Handler handler) {
             super(handler);
@@ -795,12 +761,5 @@ public class QSPanel extends ViewGroup {
             mContext.getContentResolver(), Settings.Secure.QS_USE_MAIN_TILES,
                 1, UserHandle.USER_CURRENT) == 1;
         }
-    public boolean isVoicePrefEnabled() {
-        boolean status = false;
-
-        if (carrier != null && (CarrierAppUtils.CARRIER.TELEPHONY_CARRIER_ONE == carrier)) {
-            status = true;
-        }
-        return status;
-    }
+    }	
 }
